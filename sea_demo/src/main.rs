b6b67bda5037bbd166e97fbdfb77eb5dbf487547
yu <yu.exclusive@icloud.com>
2022-05-13 11:25:14 +0800

    add sea orm

--- sea_demo/src/main.rs
+++ sea_demo/src/main.rs
@@ -11,6 +11,12 @@ use select::*;
 
 #[async_std::main]
 async fn main() {
+    tracing_subscriber::fmt()
+        .with_max_level(tracing::Level::DEBUG)
+        .with_test_writer()
+        .init();
+
+
     let url = env::var("DATABASE_URL").expect("please set DATABASE_URL");
     let db = Database::connect(url).await.unwrap();
 
@@ -21,6 +27,7 @@ async fn main() {
     all_about_select(&db).await.unwrap();
 
     println!("===== =====\n");
+
 
     // all_about_operation(&db).await.unwrap();
 }
